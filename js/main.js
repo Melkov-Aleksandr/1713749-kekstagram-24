6ed742d2a536b9969da4626ed7e47798ed7a40e7
Александр <melkov.1994@list.ru>
2021-10-30 19:04:26 +0500

    доработал 4 задание

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const NUMBER_OF_PEOPLE = 25;
 //проверка чисел
 function getRandomNumber(min, max) {
   if (min < 0 || max < 0) {
@@ -29,7 +30,7 @@ getStringLength(maxString, testString);
 
 //генерация людей
 
-const userCommentPhrase = [
+const userCommentPhrases = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -51,27 +52,6 @@ const names = [
   'Вашингтон',
 ];
 
-class comment {
-  constructor(id, avatar, messege, name) {
-    this.id = id;
-    this.avatar = avatar;
-    this.messege = messege;
-    this.name = name;
-  }
-}
-
-class element {
-  constructor(id, url, description, likes, comments) {
-    this.id = id;
-    this.url =url;
-    this.description = description;
-    this.likes = likes;
-    this.comments = comments;
-  }
-}
-
-//получаем рандомное чилосо не повторяющиеся
-
 const randomNumberId = [];
 
 function myRandom() {
@@ -85,25 +65,29 @@ function myRandom() {
   }
 }
 
-//получаем рандомный комментарий
-
 function getUserComments() {
   const userCommentArray = [];
-  for (let jam = 0; jam < getRandomNumber(1,8); jam++) {
-    const usercomment = new comment(myRandom(), `img/avatar${getRandomNumber(1, 6)}.svg`, userCommentPhrase[getRandomNumber(0, userCommentPhrase.length - 1)], names[getRandomNumber(0, names.length - 1)]);
-    userCommentArray.push(usercomment);
+  for (let index = 0; index < getRandomNumber(1,8); index++) {
+    const comment = {
+      id: myRandom(),
+      avatar: `img/avatar${getRandomNumber(1, 6)}.svg`,
+      messege: userCommentPhrases[getRandomNumber(0, userCommentPhrases.length - 1)],
+      name: names[getRandomNumber(0, names.length - 1)],
+    };
+    userCommentArray.push(comment);
   }
   return(userCommentArray);
 }
 
-const number25 = 25;
-
-//собераем массив вместе
-
-for (let iam = 0; iam < number25; iam++) {
-  const userElement = new element(iam+1, `photos/${iam+1}.jpg`, `описание ${iam+1}`, getRandomNumber(15, 200), getUserComments());
+for (let index = 0; index < NUMBER_OF_PEOPLE; index++) {
+  const userElement = {
+    id: index+1,
+    url: `photos/${index+1}.jpg`,
+    description: `описание ${index+1}`,
+    likes: getRandomNumber(15, 200),
+    comments: getUserComments(),
+  };
   list.push(userElement);
 }
 
-// console.log(list)
 //
